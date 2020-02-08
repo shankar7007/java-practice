28532ce5457bf687c1d12ffed5cff2ed57db7830
shankar7007 <patelshankar29@yahoo.com>
2020-02-08 16:33:49 +0000

    homewoprk

---
+++ src/printpractice/BioData.java
@@ -0,0 +1,11 @@
+package printpractice;
+
+public class BioData {
+
+    public static void main(String[]args){
+        System.out.println("Ravi");
+        System.out.println("Address");
+        System.out.print("Age42");
+        System.out.print("184,Hart Lane");
+    }
+}
---
+++ src/printpractice/HelloWord.java
@@ -0,0 +1,13 @@
+package printpractice;
+
+public class HelloWord {
+    public static void main(String[]args){
+        System.out.println("Hello");
+        System.out.println("World");
+
+        System.out.print("Hello ");
+        System.out.print("World");
+
+
+    }
+}
